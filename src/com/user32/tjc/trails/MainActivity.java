d09c6524575501d69e0408b1086b1c2c900242bb
tjcelaya <tjcelaya@gmail.com>
2013-11-12 20:24:20 -0500

    testing marker

--- src/com/user32/tjc/trails/MainActivity.java
+++ src/com/user32/tjc/trails/MainActivity.java
@@ -7,7 +7,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.*;
 
 import android.app.Activity;
 import android.app.Dialog;
@@ -15,6 +17,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -22,10 +25,19 @@ import android.util.Log;
 public class MainActivity extends FragmentActivity implements LocationListener {
 
 		private GoogleMap map;
+		private double currentLat;
+		private double currentLng;
+		private LatLng loveBldgLocation;
+		private boolean markerSet;
+		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_main);
+	        
+	        loveBldgLocation = new LatLng(30.4461,-84.2993);
+	        markerSet = false;
+	        
 	        // Getting Google Play availability status
 	        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
@@ -62,25 +74,70 @@ public class MainActivity extends FragmentActivity implements LocationListener {
 	            if(location!=null){
 	                onLocationChanged(location);
 	            }
+	            
+	        	Log.w("Trails", "made it to check");
+
+	            
+	            
+		        if (markerSet == false) {
+		        	Log.w("Trails", "setting marker since we haven't");
+		            map.addMarker(new MarkerOptions()
+			            .position(loveBldgLocation)
+			            .title("Love!"));
+		            markerSet = true;
+		        }
+	            
 	            locationManager.requestLocationUpdates(provider, 20000, 0, this);
+	            
+		        map.moveCamera(CameraUpdateFactory.newLatLng(loveBldgLocation));
+
+		        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+		        
+				new DelayedZoom().execute();
+
 	        }
 	    }
+		
+		private class DelayedZoom extends AsyncTask<Void, Void, Void> {
+		
+		    @Override
+		    protected Void doInBackground(Void... v) {
+	                try {
+	                    Thread.sleep(1000);
+	                } catch (InterruptedException e) {
+	                    // TODO Auto-generated catch block
+	                    e.printStackTrace();
+	                }
+	                return null;
+		    }        
+		
+		    @Override
+		    protected void onPostExecute(Void v) {             
+		    }
+		
+		}
 		
 	    @Override
 	    public void onLocationChanged(Location location) {
 
 	        // Getting latitude of the current location
-	        double latitude = location.getLatitude();
+	        currentLat = location.getLatitude();
 
 	        // Getting longitude of the current location
-	        double longitude = location.getLongitude();
+	        currentLng = location.getLongitude();
 
 	        // Creating a LatLng object for the current location
-	        LatLng latLng = new LatLng(latitude, longitude);
+	        LatLng latLng = new LatLng(currentLat, currentLng);
 
 	        // Showing the current location in Google Map
-	        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
+	        LatLngBounds bounds = new LatLngBounds.Builder()
+	        	.include(latLng)
+	        	.include(loveBldgLocation)
+	        	.build();
+	        
+	        map.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20));
+	        
 	        // Zoom in the Google Map
 	        map.animateCamera(CameraUpdateFactory.zoomTo(15));
 	    }
